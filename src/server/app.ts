df0de542d825ddc2660a642bed51f8cb95c74892
Raimon <raimon.txollo@gmail.com>
2025-04-16 00:09:48 +0200

    Add handlerEndpointNotFound middleware, its test and endpoint

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import checkHealthStatus from "./middleware/checkHealthStatus.js";
+import handlerEndpointNotFound from "./middleware/handlerEndpointNotFound.js";
 
 const app = express();
 
 app.get("/", checkHealthStatus);
+
+app.use(handlerEndpointNotFound);
 
 export default app;
