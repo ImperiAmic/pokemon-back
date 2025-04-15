367e7fb1a4f1bb093eec7cdeb62c057d967f1f87
Raimon <raimon.txollo@gmail.com>
2025-04-16 00:14:20 +0200

    Install morgan

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 import checkHealthStatus from "./middleware/checkHealthStatus.js";
 import handlerEndpointNotFound from "./middleware/handlerEndpointNotFound.js";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", checkHealthStatus);
 
